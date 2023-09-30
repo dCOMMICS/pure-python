bcbd1d60831f484445ea48c4fd5dfa47433b2113
dCOMMICS <sanwurld@gmail.com>
2023-09-30 19:46:43 +0300

    lately ooh

---
+++ whiteboard-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+        
+        <p>
+          Welcome On Board
+        </p>
+        
+        
+    </div>
+  );
+}
+
+export default App;
