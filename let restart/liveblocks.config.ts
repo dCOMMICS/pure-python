bc607ac646813461933ec3e286e3f3b3a810b08a
dCOMMICS <sanwurld@gmail.com>
2023-09-30 23:29:20 +0300

    cannot find module '@liveblocks/client' or its corresponding type declaration

---
+++ let restart/liveblocks.config.ts
@@ -0,0 +1,39 @@
+import { createClient } from "@liveblocks/client";
+import { createRoomContext } from "@liveblocks/react";
+
+const client = createClient({
+  publicApiKey: "pk_dev_m-45O96qzAZfzkGPN0iHx89hcaZvyXIz__R-068wScX37JT51MjWAXlGYV3dspro",
+});
+
+type Presence = {};
+type Storage = {};
+type UserMeta = {};
+type RoomEvent = {};
+
+export const {
+  suspense: {
+    // RoomProvider,
+    useRoom,
+    useMyPresence,
+    useUpdateMyPresence,
+    useSelf,
+    useOthers,
+    useOthersMapped,
+    useOthersConnectionIds,
+    useOther,
+    useBroadcastEvent,
+    useEventListener,
+    useErrorListener,
+    useStorage,
+    useObject,
+    useMap,
+    useList,
+    useBatch,
+    useHistory,
+    useUndo,
+    useRedo,
+    useCanUndo,
+    useCanRedo,
+    useMutation,
+  },
+} = createRoomContext<Presence, Storage, UserMeta, RoomEvent>(client);
