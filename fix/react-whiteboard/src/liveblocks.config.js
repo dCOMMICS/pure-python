ce912169d7d90f9c173cd958170fabf48efd82cb
dCOMMICS <sanwurld@gmail.com>
2023-09-30 23:26:00 +0300

    trying a new connection

--- fix/react-whiteboard/src/liveblocks.config.js
+++ fix/react-whiteboard/src/liveblocks.config.js
@@ -39,19 +39,20 @@
 //   useBatch,
 // } = createRoomContext(client);
 
-import { createClient } from "@liveblocks/client";
-import { createRoomContext } from "@liveblocks/react";
+// import { createClient } from "@liveblocks/client";
+// import { createRoomContext } from "@liveblocks/react";
 
-const client = createClient({
-  publicApiKey: "pk_dev_0ScUkUr8OnTlJJxHBnyxIez0wK73iQhFSMmfuG6UMh32f2RwAJFqWXNuZg_dQ3Og",
-});
+// const client = createClient({
+//   publicApiKey: "pk_dev_0ScUkUr8OnTlJJxHBnyxIez0wK73iQhFSMmfuG6UMh32f2RwAJFqWXNuZg_dQ3Og",
+// });
 
-export const {
-  RoomProvider,
-  useOthers,
-  useMyPresence,
-  useMap,
-  useHistory,
-  useBatch,
-  useRoom,
-} = createRoomContext(client);
+// export const {
+//   RoomProvider,
+//   useOthers,
+//   useMyPresence,
+//   useMap,
+//   useHistory,
+//   useBatch,
+//   useRoom,
+// } = createRoomContext(client);
+
