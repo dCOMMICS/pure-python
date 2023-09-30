fbc1acc17a1f95cbc4c0653de7b1604ee905ef76
dCOMMICS <sanwurld@gmail.com>
2023-09-30 23:15:30 +0300

    errors everywhere

--- fix/react-whiteboard/src/liveblocks.config.js
+++ fix/react-whiteboard/src/liveblocks.config.js
@@ -38,3 +38,20 @@
 //   useHistory,
 //   useBatch,
 // } = createRoomContext(client);
+
+import { createClient } from "@liveblocks/client";
+import { createRoomContext } from "@liveblocks/react";
+
+const client = createClient({
+  publicApiKey: "pk_dev_0ScUkUr8OnTlJJxHBnyxIez0wK73iQhFSMmfuG6UMh32f2RwAJFqWXNuZg_dQ3Og",
+});
+
+export const {
+  RoomProvider,
+  useOthers,
+  useMyPresence,
+  useMap,
+  useHistory,
+  useBatch,
+  useRoom,
+} = createRoomContext(client);
