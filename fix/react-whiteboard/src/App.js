32fbc38c2fedd41db864bfd5e80d0d563ff50311
dCOMMICS <sanwurld@gmail.com>
2023-09-30 23:04:45 +0300

    export not found

--- fix/react-whiteboard/src/App.js
+++ fix/react-whiteboard/src/App.js
@@ -1,61 +1,108 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-import { useMap, useMyPresence, useOthers } from "./liveblocks.config";
+// import { useMap, useMyPresence, useOthers } from "./liveblocks.config";
+
+// /* ... */
+
+// function Canvas({ shapes }) {
+//   const [{ selectedShape }, setPresence] = useMyPresence();
+//   const others = useOthers();
+
+//   /* ... */
+
+//   const onShapePointerDown = (e, shapeId) => {
+//     setPresence({ selectedShape: shapeId });
+//   };
+
+//   return (
+//     <>
+//       <div className="canvas">
+//         {Array.from(shapes, ([shapeId, shape]) => {
+//           let selectionColor =
+//             selectedShape === shapeId
+//               ? "blue"
+//               : others
+//                   .toArray()
+//                   .some((user) => user.presence?.selectedShape === shapeId)
+//               ? "green"
+//               : undefined;
+//           return (
+//             <Rectangle
+//               key={shapeId}
+//               shape={shape}
+//               id={shapeId}
+//               onShapePointerDown={onShapePointerDown}
+//               selectionColor={selectionColor}
+//             />
+//           );
+//         })}
+//       </div>
+//       <div className="toolbar">
+//         <button onClick={insertRectangle}>Rectangle</button>
+//       </div>
+//     </>
+//   );
+// }
+
+// const Rectangle = ({ shape, id, onShapePointerDown, selectionColor }) => {
+//   const { x, y, fill } = shape;
+
+//   return (
+//     <div
+//       onPointerDown={(e) => onShapePointerDown(e, id)}
+//       className="rectangle"
+//       style={{
+//         transform: `translate(${x}px, ${y}px)`,
+//         backgroundColor: fill ? fill : "#CCC",
+//         borderColor: selectionColor || "transparent",
+//       }}
+//     ></div>
+//   );
+// };
+
+import { useEffect, useState } from "react";
+import {
+  useMap,
+  useMyPresence,
+  useOthers,
+  useHistory,
+} from "./liveblocks.config";
 
 /* ... */
 
 function Canvas({ shapes }) {
-  const [{ selectedShape }, setPresence] = useMyPresence();
-  const others = useOthers();
+  const history = useHistory();
 
   /* ... */
 
   const onShapePointerDown = (e, shapeId) => {
-    setPresence({ selectedShape: shapeId });
+    e.stopPropagation();
+
+    setPresence({ selectedShape: shapeId }, { addToHistory: true });
+
+    setIsDragging(true);
   };
+
+  const onCanvasPointerUp = (e) => {
+    if (!isDragging) {
+      setPresence({ selectedShape: null }, { addToHistory: true });
+    }
+
+    setIsDragging(false);
+  };
+
+  /* ... */
 
   return (
     <>
-      <div className="canvas">
-        {Array.from(shapes, ([shapeId, shape]) => {
-          let selectionColor =
-            selectedShape === shapeId
-              ? "blue"
-              : others
-                  .toArray()
-                  .some((user) => user.presence?.selectedShape === shapeId)
-              ? "green"
-              : undefined;
-          return (
-            <Rectangle
-              key={shapeId}
-              shape={shape}
-              id={shapeId}
-              onShapePointerDown={onShapePointerDown}
-              selectionColor={selectionColor}
-            />
-          );
-        })}
-      </div>
+      {/* ... */}
       <div className="toolbar">
-        <button onClick={insertRectangle}>Rectangle</button>
+        {/* ... */}
+        <button onClick={history.undo}>Undo</button>
+        <button onClick={history.redo}>Redo</button>
       </div>
     </>
   );
 }
 
-const Rectangle = ({ shape, id, onShapePointerDown, selectionColor }) => {
-  const { x, y, fill } = shape;
-
-  return (
-    <div
-      onPointerDown={(e) => onShapePointerDown(e, id)}
-      className="rectangle"
-      style={{
-        transform: `translate(${x}px, ${y}px)`,
-        backgroundColor: fill ? fill : "#CCC",
-        borderColor: selectionColor || "transparent",
-      }}
-    ></div>
-  );
-};
+/* Rectangle */
