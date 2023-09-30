0854977517a612bc6e45011597601834a54579e0
dCOMMICS <sanwurld@gmail.com>
2023-09-30 23:04:00 +0300

    'client' is assigned a value but never used

--- fix/react-whiteboard/src/liveblocks.config.js
+++ fix/react-whiteboard/src/liveblocks.config.js
@@ -4,6 +4,15 @@
 //   publicApiKey: "pk_dev_0ScUkUr8OnTlJJxHBnyxIez0wK73iQhFSMmfuG6UMh32f2RwAJFqWXNuZg_dQ3Og",
 // });
 
+// import { createClient } from "@liveblocks/client";
+// import { createRoomContext } from "@liveblocks/react";
+
+// const client = createClient({
+//   publicApiKey: "pk_dev_0ScUkUr8OnTlJJxHBnyxIez0wK73iQhFSMmfuG6UMh32f2RwAJFqWXNuZg_dQ3Og",
+// });
+
+// export const { RoomProvider, useMap } = createRoomContext(client);
+
 import { createClient } from "@liveblocks/client";
 import { createRoomContext } from "@liveblocks/react";
 
@@ -11,5 +20,6 @@ const client = createClient({
   publicApiKey: "pk_dev_0ScUkUr8OnTlJJxHBnyxIez0wK73iQhFSMmfuG6UMh32f2RwAJFqWXNuZg_dQ3Og",
 });
 
-export const { RoomProvider, useMap } = createRoomContext(client);
+export const { RoomProvider, useOthers, useMyPresence, useMap, useHistory } =
+  createRoomContext(client);
 
