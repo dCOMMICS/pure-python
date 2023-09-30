bb60ee042085a9a01d80ecb6804fb194da68bbc2
dCOMMICS <sanwurld@gmail.com>
2023-09-30 20:49:38 +0300

    drawing shapes

--- fix/react-whiteboard/src/App.js
+++ fix/react-whiteboard/src/App.js
@@ -2,38 +2,42 @@ import { useMap } from "./liveblocks.config";
 
 import "./App.css";
 
-export default function App() {
-  const shapes = useMap("shapes");
+const COLORS = ["#DC2626", "#D97706", "#059669", "#7C3AED", "#DB2777"];
 
-  if (shapes == null) {
-    return <div className="loading">Loading</div>;
-  }
-
-  return <Canvas shapes={shapes} />;
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
 
+function getRandomColor() {
+  return COLORS[getRandomInt(COLORS.length)];
+}
+
+/* App */
+
 function Canvas({ shapes }) {
+  const insertRectangle = () => {
+    const shapeId = Date.now().toString();
+    const rectangle = {
+      x: getRandomInt(300),
+      y: getRandomInt(300),
+      fill: getRandomColor(),
+    };
+    shapes.set(shapeId, rectangle);
+  };
+
   return (
     <>
       <div className="canvas">
         {Array.from(shapes, ([shapeId, shape]) => {
+          // eslint-disable-next-line react/jsx-no-undef
           return <Rectangle key={shapeId} shape={shape} />;
         })}
+      </div>
+      <div className="toolbar">
+        <button onClick={insertRectangle}>Rectangle</button>
       </div>
     </>
   );
 }
 
-const Rectangle = ({ shape }) => {
-  const { x, y, fill } = shape;
-
-  return (
-    <div
-      className="rectangle"
-      style={{
-        transform: `translate(${x}px, ${y}px)`,
-        backgroundColor: fill ? fill : "#CCC",
-      }}
-    ></div>
-  );
-};
+/* Rectangle */
